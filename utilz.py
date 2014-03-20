81f5776fb7496d740f4fb1fb2e395550cbf6cdd4
arendu <adithya.r@gmail.com>
2014-03-20 18:31:10 -0400

    added stop words to utilz

--- utilz.py
+++ utilz.py
@@ -2,8 +2,13 @@ __author__ = 'arenduchintala'
 import nltk
 import string
 
+STOP_WORDS = "a an and are as at be by for from has he in is it its of on that the to was were will with".split()
+VEC_LEN = 100
+
 
 def tokenize(s):
     #s = "string. With. Punctuation?" # Sample string
-    out = ''.join([i for i in s if (i not in string.punctuation) and (i in string.printable)])
+    #out = ''.join([i for i in s if (i not in string.punctuation) and (i in string.printable)])
+    s = s.replace('&quot;', '')
+    out = ''.join([i for i in s if i in string.printable])
     return nltk.word_tokenize(out.lower())
